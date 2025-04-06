c0aa661adaa8b24e0b6a49947c22e5cb4fda09ef
SarahTabada <143475771+SarahTabada@users.noreply.github.com>
2025-04-05 20:27:15 -0500

    Create login.tsx

---
+++ login.tsx
@@ -0,0 +1,20 @@
+"use client";
+import * as React from "react";
+
+/**
+ * Login component that displays a login image
+ * @returns Login component with image
+ */
+function Login() {
+  return (
+    <section className="login-container">
+      <img
+        src="https://cdn.builder.io/api/v1/image/assets/TEMP/5fc556500c9ec994e0017d7bb7aa7049a9d6cee6?placeholderIfAbsent=true&apiKey=09643f8f95f343fbb6aaddde168a25c7"
+        alt="Login visual"
+        className="object-contain w-full aspect-[0.69]"
+      />
+    </section>
+  );
+}
+
+export default Login;
